732d9261d7ba1fa8307cdb58d002ffef8404b759
ChissgaN <coderchissgan@gmail.com>
2025-07-05 18:49:11 -0600

    Implement exercises for user input and processing: name greeting, grade calculation, number guessing, statistics, and squaring a number.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,65 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter your grande porcentage: ");
+        int percentage = int.Parse(Console.ReadLine());
+
+        string letter = "";
+        string sign = "";
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Determine if passed
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("Don't give up! Try again next time.");
+        }
+
+        // Stretch Challenge: add + or -
+        int lastDigit = percentage % 10;
+
+        if (letter != "F" && letter != "A")
+        {
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+        else if (letter == "A")
+        {
+            if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+
+        Console.WriteLine($"Your grade is: {letter}{sign}");
     }
 }
